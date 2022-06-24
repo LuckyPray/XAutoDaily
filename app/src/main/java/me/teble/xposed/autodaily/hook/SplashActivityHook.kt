d5497eba494afa2dcb52773624fd0a372df8e283
teble <xie091030@qq.com>
2022-06-24 17:23:59 +0800

    修改更新提示的dialog实现

--- app/src/main/java/me/teble/xposed/autodaily/hook/SplashActivityHook.kt
+++ app/src/main/java/me/teble/xposed/autodaily/hook/SplashActivityHook.kt
@@ -1,17 +1,18 @@
 package me.teble.xposed.autodaily.hook
 
 import android.app.Activity
-import android.view.Display
-import android.view.WindowManager
-import android.view.WindowManager.LayoutParams.FLAG_BLUR_BEHIND
-import android.view.WindowManager.LayoutParams.FLAG_DIM_BEHIND
-import androidx.compose.ui.platform.ComposeView
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import me.teble.xposed.autodaily.activity.module.ModuleActivity
+import me.teble.xposed.autodaily.config.GITHUB_RELEASE_URL
+import me.teble.xposed.autodaily.config.PAN_URL
 import me.teble.xposed.autodaily.config.SplashActivity
 import me.teble.xposed.autodaily.hook.CoreServiceHook.Companion.AUTO_EXEC
 import me.teble.xposed.autodaily.hook.CoreServiceHook.Companion.handler
@@ -21,10 +22,7 @@ import me.teble.xposed.autodaily.hook.base.ProcUtil
 import me.teble.xposed.autodaily.task.util.ConfigUtil
 import me.teble.xposed.autodaily.task.util.ConfigUtil.loadSaveConf
 import me.teble.xposed.autodaily.task.util.formatDate
-import me.teble.xposed.autodaily.ui.AppUpdateLayout
 import me.teble.xposed.autodaily.ui.ConfUnit
-import me.teble.xposed.autodaily.ui.ConfigUpdateLayout
-import me.teble.xposed.autodaily.ui.CustomDialog
 import java.util.*
 
 class SplashActivityHook : BaseHook() {
@@ -48,83 +46,105 @@ class SplashActivityHook : BaseHook() {
                         ConfUnit.needUpdate = true
                     }
                 }
-                withContext(Dispatchers.Main) {
-                    if (ConfUnit.needUpdate) {
-                        context.openAppUpdateDialog()
-                    } else if (ConfUnit.needShowUpdateLog) {
-                        context.openConfigUpdateLog()
-                        ConfUnit.needShowUpdateLog = false
-                    }
-                    handler.sendEmptyMessageDelayed(AUTO_EXEC, 10_000)
+                if (ConfUnit.needUpdate) {
+                    context.openAppUpdateDialog()
+                } else if (ConfUnit.needShowUpdateLog) {
+                    context.openConfigUpdateLog()
+                    ConfUnit.needShowUpdateLog = false
                 }
+                handler.sendEmptyMessageDelayed(AUTO_EXEC, 10_000)
             }
         }
 
         findMethod(SplashActivity) { name == "doOnStart" }.hookAfter {
             val context = it.thisObject as Activity
             scope.launch {
-                withContext(Dispatchers.Main) {
-                    if (ConfUnit.needUpdate) {
-                        context.openAppUpdateDialog()
-                    } else if (ConfUnit.needShowUpdateLog) {
-                        context.openConfigUpdateLog()
-                        ConfUnit.needShowUpdateLog = false
+                if (ConfUnit.needUpdate) {
+                    context.openAppUpdateDialog()
+                } else if (ConfUnit.needShowUpdateLog) {
+                    context.openConfigUpdateLog()
+                    ConfUnit.needShowUpdateLog = false
+                }
+            }
+        }
+    }
+}
+
+lateinit var appUpdateDialog: AlertDialog
+
+suspend fun Activity.openAppUpdateDialog() {
+    withContext(Dispatchers.IO) {
+        var builder: AlertDialog.Builder? = null
+        val isInitialized = ::appUpdateDialog.isInitialized
+        if (!isInitialized) {
+            builder = AlertDialog.Builder(this@openAppUpdateDialog, 5).apply {
+                setTitle("检测到新版本")
+                val updateList = ConfUnit.versionInfoCache?.updateLog ?: emptyList()
+                val updateLog = buildList {
+                    updateList.forEach {
+                        if (it.isNotEmpty()) {
+                            add(it)
+                        }
                     }
+                }.toTypedArray()
+                setItems(updateLog, null)
+                setNeutralButton("今日不再提醒") { _, _ ->
+                    ConfUnit.blockUpdateOneDay = Date().formatDate()
                 }
+                setNegativeButton("蓝奏云") { _, _ ->
+                    context.startActivity(Intent().apply {
+                        action = Intent.ACTION_VIEW
+                        data = Uri.parse(PAN_URL)
+                    })
+                }
+                setPositiveButton("github") { _, _ ->
+                    context.startActivity(Intent().apply {
+                        action = Intent.ACTION_VIEW
+                        data = Uri.parse(GITHUB_RELEASE_URL)
+                    })
+                }
+            }
+        }
+        withContext(Dispatchers.Main) {
+            if (!isInitialized) {
+                appUpdateDialog = builder!!.create()
+            }
+            if (!appUpdateDialog.isShowing && !isFinishing) {
+                appUpdateDialog.show()
             }
         }
     }
 }
 
-@Suppress("DEPRECATION")
-fun Activity.openAppUpdateDialog() {
-    this.runOnUiThread {
-        val dialog = CustomDialog(this).apply {
-            val dialog = this
-            val windowManager: WindowManager = this@openAppUpdateDialog.windowManager
-            val display: Display = windowManager.defaultDisplay
-            val lp = window?.attributes
-            lp?.width = (display.width * 0.8).toInt()
-            lp?.height = (display.height * 0.6).toInt()
-            lp?.dimAmount = 0.42f
-            window?.addFlags(FLAG_BLUR_BEHIND)
-            window?.addFlags(FLAG_DIM_BEHIND)
-            val view = ComposeView(context).apply {
-                setContent {
-                    AppUpdateLayout(dialog)
-                }
-            }
-            setContentView(view)
-        }
-        val skipShow = ConfUnit.blockUpdateOneDay == Date().formatDate()
-        if (!dialog.isShowing && !isFinishing && !skipShow) {
-            dialog.show()
-        }
-    }
-}
+lateinit var configUpdateDialog: AlertDialog
 
-@Suppress("DEPRECATION")
-fun Activity.openConfigUpdateLog() {
-    this.runOnUiThread {
-        val dialog = CustomDialog(this).apply {
-            val dialog = this
-            val windowManager: WindowManager = this@openConfigUpdateLog.windowManager
-            val display: Display = windowManager.defaultDisplay
-            val lp = window?.attributes
-            lp?.width = (display.width * 0.8).toInt()
-            lp?.height = (display.height * 0.6).toInt()
-            lp?.dimAmount = 0.42f
-            window?.addFlags(FLAG_BLUR_BEHIND)
-            window?.addFlags(FLAG_DIM_BEHIND)
-            val view = ComposeView(context).apply {
-                setContent {
-                    ConfigUpdateLayout(dialog)
+suspend fun Activity.openConfigUpdateLog() {
+    withContext(Dispatchers.IO) {
+        var builder: AlertDialog.Builder? = null
+        val isInitialized = ::configUpdateDialog.isInitialized
+        if (!isInitialized) {
+            builder = AlertDialog.Builder(this@openConfigUpdateLog, 5).apply {
+                setTitle("配置更新日志")
+                val updateLog = buildList {
+                    loadSaveConf().updateLogs.forEach {
+                        add("v${it.version}:\n${it.desc}")
+                    }
+                }.reversed().toTypedArray()
+                setItems(updateLog, null)
+                setNegativeButton("确定") { _, _ -> }
+                setPositiveButton("前往配置") { _, _ ->
+                    val intent = Intent(context, ModuleActivity::class.java)
+                    context.startActivity(intent)
                 }
             }
-            setContentView(view)
         }
-        if (!dialog.isShowing && !isFinishing) {
-            dialog.show()
+        withContext(Dispatchers.Main) {
+            if (!isInitialized) {
+                configUpdateDialog = builder!!.create()
+            }
+            if (!configUpdateDialog.isShowing && !isFinishing) {
+                configUpdateDialog.show()
+            }
         }
     }
 }
