82c07f66db74672fa05632e1a90c14b841b424db
fankesyooni <qzmmcn@163.com>
2023-02-22 05:19:59 +0800

    fix: inject module resources maybe problem in some internal Hook Framework

--- app/src/main/java/me/teble/xposed/autodaily/hook/proxy/activity/ResInjectUtil.kt
+++ app/src/main/java/me/teble/xposed/autodaily/hook/proxy/activity/ResInjectUtil.kt
@@ -1,4 +1,5 @@
 @file:JvmName("ResInjectUtil")
+
 package me.teble.xposed.autodaily.hook.proxy.activity
 
 import android.content.res.Resources
@@ -9,11 +10,13 @@ import me.teble.xposed.autodaily.utils.LogUtil
 import me.teble.xposed.autodaily.utils.invoke
 
 fun injectRes(res: Resources = hostContext.resources) {
-    try {
-        res.getString(R.string.res_inject_success)
-        return
-    } catch (ignored: Resources.NotFoundException) {
-    }
+    // FIXME: 去除资源注入成功检测，每次重复注入资源，可以修复一些内置 Hook 框架注入虽然成功但是依然找不到资源 ID 的问题
+    //        复现：梦境框架、应用转生、LSPatch，QQ 版本 8.3.9、8.4.1
+    //    try {
+    //        res.getString(R.string.res_inject_success)
+    //        return
+    //    } catch (ignored: Resources.NotFoundException) {
+    //    }
     LogUtil.log("Module path = $modulePath")
     //-----资源注入部分-----
     val assets = res.assets
