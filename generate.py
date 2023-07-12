83ecc11b195e8bbf8525a3b8c516bbe4873b2df3
韵の祈 <33313113+teble@users.noreply.github.com>
2023-07-12 16:58:57 +0800

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+
+src = {
+    "app": "app-meta.json",
+    "config": "conf-meta.json",
+    "notice": "notice",
+}
+
+for key, value in src.items():
+    data = None
+    if os.path.exists(value):
+        with open(value, 'r') as f:
+            data = f.read()
+    if data and value.endswith(".json"):
+        data = json.loads(data)
+    src[key] = data
+
+with open('meta.json', 'w') as f:
+    f.write(json.dumps(src, ensure_ascii=False, separators=(',', ':')))
