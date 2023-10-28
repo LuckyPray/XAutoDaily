737d642b802d8e68d1e7565d5d52ba6f9148201b
teble <me@teble.me>
2023-10-28 23:42:22 +0800

    删除部分无用常量

--- app/src/main/java/me/teble/xposed/autodaily/config/Constants.kt
+++ app/src/main/java/me/teble/xposed/autodaily/config/Constants.kt
@@ -19,11 +19,6 @@ const val ALIPAY_QRCODE = "tsx060790flcdxvkynija2e"
 //    String TEST_API_URL = BuildConfig.TEST_API_URL;
 const val TG_URL = "tg://resolve?domain=XAutoDaily"
 const val GROUP_URL = ""
-const val ALIPAY_QRCODE_PNG_URL =
-    "https://ae01.alicdn.com/kf/H1238e3de8a2949b18bb6a2d1197ec8f2H.png"
+const val ALIPAY_QRCODE_PNG_URL = "https://ae01.alicdn.com/kf/H1238e3de8a2949b18bb6a2d1197ec8f2H.png"
 
-const val XA_API_URL = "http://daily.huasteble.cn"
-const val VERSION = "/version"
-const val NOTICE = "/notice"
-const val QZONE = "/qzone"
 const val qqUserAgent = "Mozilla/5.0 (Linux; Android 12; Mi 10 Pro Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/046213 Mobile Safari/537.36 V1_AND_SQ_8.9.15_3408_YYB_D A_8091500 QQ/8.9.15.9425 NetType/WIFI WebP/0.3.0 AppId/537138832 Pixel/1080 StatusBarHeight/90 SimpleUISwitch/0 QQTheme/999 StudyMode/0 CurrentMode/0 CurrentFontScale/1.0 GlobalDensityScale/0.9818181 AllowLandscape/false InMagicWin/0"
