8ba146b13613cb2d7036c159d2edf2dce738e199
teble <me@teble.me>
2024-05-10 15:51:35 +0800

    修复部分系统Toast.makeText中text参数强制为NonNull导致的空指针异常

--- app/src/main/java/me/teble/xposed/autodaily/hook/utils/ToastUtil.kt
+++ app/src/main/java/me/teble/xposed/autodaily/hook/utils/ToastUtil.kt
@@ -1,11 +1,13 @@
 package me.teble.xposed.autodaily.hook.utils
 
 import android.content.Context
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 import me.teble.xposed.autodaily.config.NAME
 import me.teble.xposed.autodaily.hook.base.hostContext
+import java.util.Locale
 
 object ToastUtil {
 
@@ -18,7 +20,16 @@ object ToastUtil {
     }
 
     private fun toast(context: Context, msg: String, longDuration: Boolean) {
-        val toast = Toast.makeText(context, null, parse(longDuration))
+        val toast: Toast
+        if (Build.BRAND.lowercase(Locale.getDefault()) == "xiaomi") {
+            // 小米系统需要使用该方式避免 Toast 弹窗附带宿主应用名前缀
+            toast = Toast.makeText(context, null, parse(longDuration))
+        } else {
+            // 部分系统会因为魔改 makeText 中的 text 参数为 @NonNull 从而导致空指针异常
+            toast = Toast(context).apply {
+                duration = parse(longDuration)
+            }
+        }
         toast.setText("$NAME: $msg")
         toast.show()
     }
