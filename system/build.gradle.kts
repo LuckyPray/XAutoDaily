65f278d5ac480eb3910b6a02d4988a1c7939bcc8
teble <me@teble.me>
2025-04-08 16:03:01 +0800

    修复编译错误

--- system/build.gradle.kts
+++ system/build.gradle.kts
@@ -26,14 +26,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_21)
-        targetCompatibility(JavaVersion.VERSION_21)
-    }
-    kotlin {
-        jvmToolchain(libs.versions.jvm.target.get().toInt())
+        sourceCompatibility(JavaVersion.VERSION_11)
+        targetCompatibility(JavaVersion.VERSION_11)
     }
 }
+
 kotlin {
+    jvmToolchain(libs.versions.jvm.target.get().toInt())
     sourceSets.all {
         languageSettings {
             enableLanguageFeature("ContextReceivers")
